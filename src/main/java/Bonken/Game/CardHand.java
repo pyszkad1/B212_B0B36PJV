7fb8e6d19ed5889be1ce9cbfbb05211413e9f997
pyszkad1 <pyszkad1@fel.cvut.cz>
2022-04-05 20:14:26 +0200

    CardHand push

--- src/main/java/Bonken/Game/CardHand.java
+++ src/main/java/Bonken/Game/CardHand.java
@@ -1,13 +1,36 @@
 package Bonken.Game;
 
-public class CardHand {
-    Card[] hand;
-    Card[] playableCards;
+import java.util.ArrayList;
 
-    public CardHand() {
+public class CardHand {
+    ArrayList<Card> hand;
+    ArrayList<Card> playableCards;
+
+    public CardHand(ArrayList<Card> hand) {
+        this.hand = hand;
     }
 
-    public void play() {
+    public ArrayList<Card> getPlayableCards(Card firstCard) {
+        playableCards = hasThisSuit(firstCard);
+        if (playableCards.size() == 0){
+            playableCards = hand;
+        }
+
+        return playableCards;
+    }
+
+    public ArrayList<Card> hasThisSuit(Card card){
+        ArrayList<Card> ret = new ArrayList<Card>();
+        for (int i = 0; i < hand.size(); i++) {
+            if (card.suit == hand.get(i).suit){
+                ret.add(hand.get(i));
+            }
+        }
+        return ret;
+    }
+
+    public Card play() {
+        return playableCards.get(playableCards.size()-1);
     }
 
 }
