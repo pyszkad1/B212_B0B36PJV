926da596597b5c85cb4dab34268b5ccdce71d9d7
pyszkad1 <pyszkad1@fel.cvut.cz>
2022-04-07 23:31:01 +0200

    working round no minigame choosing

--- src/main/java/Bonken/Game/Round.java
+++ src/main/java/Bonken/Game/Round.java
@@ -1,7 +1,19 @@
 package Bonken.Game;
 
+import java.util.ArrayList;
+
 public class Round {
+    CardHand[] cardHands;
+
     public Round() {
+        Deck deck = new Deck(); //TODO later do in game!!!
+        deck.shuffle();
+        ArrayList<ArrayList<Card>> hands = deck.deal();
+        System.out.println(hands.toString());
+        cardHands = new CardHand[4];
+        for (int i = 0; i < 4; i++) {
+            cardHands[i] = new CardHand(hands.get(i));
+        }
 
     }
 
@@ -9,9 +21,18 @@ public class Round {
         //TODO
     }
 
-    public void playTrick(int firstPlayer) {
-        Trick trick = new Trick()
+    public void playRound() {
+        int firstPlayer = 0; //TODO should be player -1 from chooser of minigame
+        for (int i = 0; i < 13; i++) {
+            firstPlayer = playTrick(firstPlayer);
+            System.out.println("first player is " + firstPlayer);
+        }
 
+    }
+
+    public int playTrick(int firstPlayer) {
+        Trick trick = new Trick(firstPlayer, cardHands, 1);
+        return trick.getTrickWinner(trick.getTrick());
     }
 
 
