976941a47e5e8276779bf4eed8fd5375b6769d9e
orendkar <orendkar@fel.cvut.cz>
2022-05-08 18:06:05 +0200

    delete css files

--- src/main/java/bonken/game/Main.java
+++ src/main/java/bonken/game/Main.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
         Game game = new Game(Integer.parseInt(args[0]));                // TODO change config
         game.startGame();
 
